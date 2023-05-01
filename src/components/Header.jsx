31182a2a43c35b09aee530dd4289dd1519e2c3ab
Nagib Mahfuz <120543584+nagibgithub@users.noreply.github.com>
2023-05-02 02:00:44 +0600

    Header section adding nav bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {NavLink} from 'react-router-dom';
 
 const Header = () => {
+
+    const [nav, setNav] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/nav')
+            .then(res => res.json())
+            .then(data => setNav(data))
+    }, [])
+
     return (
         <div>
-            this is header
+            {
+                nav.map(pd => <NavLink key={pd.id} to={pd.path} >{pd.title}</NavLink>)
+            }
         </div>
     );
 };
